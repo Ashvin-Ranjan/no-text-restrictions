9674117d5bde05824ed404b46c240a23c7028c51
Ash Ranjan <ranjanashvin@gmail.com>
2021-08-09 11:57:52 -0700

    Server Side Stuff

--- src/main/java/vin/ash/nores/sharedstates/SharedStates.java
+++
@@ -1,9 +0,0 @@
-package vin.ash.nores.sharedstates;
-
-import java.util.HashSet;
-
-public class SharedStates {
-    public static HashSet<Character> charactersAllowed = new HashSet<>();
-
-    public static boolean allowAll = false;
-}
---
+++ src/main/java/vin/ash/noresserver/NoResMod.java
@@ -0,0 +1,9 @@
+package vin.ash.noresserver;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoResMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
